ca05e000431ad078437740ca8f78b64cf951798e
tsheporamantso <tshepo.ramantso@outlook.com>
2023-03-27 12:39:35 +0200

    Add Javacsript functionality and import modules

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 import { DateTime } from './modules/luxon.js';
+import Book from './modules/userinterface.js';
+import { navBar } from './modules/navigation.js';
 
-const currentDate = DateTime.now().toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
+document.addEventListener('DOMContentLoaded', () => {
+  const book = new Book();
+  book.init();
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  navBar();
+});
+
+const currentDate = DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
 document.getElementById('timeText').innerHTML = currentDate;
